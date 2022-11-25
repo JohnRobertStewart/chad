67fa1321edcf13be325aadf908b836980b814fee
John Stewart <johnrobertstewart@protonmail.com>
2022-11-24 17:34:25 -0800

    not working on other browsers

--- myapp/src/Footer.jsx
+++ myapp/src/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className = 'footer font-bubble absolute bottom-12 right-12 '>
       <div>
       buy something would ya
-      <img className="shirt font-bubble justify-right" src={shirt} alt="cool tie die gear"/>
+      <img className="shirt font-bubble justify-right" src={shirt} alt="cool tie die gear" href="https://tinyhandztyedyez.com/" target="_blank" rel="noopener noreferrer"/>
 
       </div>
     </div>
