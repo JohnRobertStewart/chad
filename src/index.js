e72ad418d15e3d594f228faea5d74585a5025f72
John Stewart <johnrobertstewart@protonmail.com>
2022-11-28 00:00:41 -0800

    had some NPM errors, fixed them - is running properly locally

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import "./App.css"
 import App from './App';
-//import Nav from './components'
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+
   <React.StrictMode>
     <App
  
